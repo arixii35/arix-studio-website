a61399bdfd785e723b157f6d9cd567d730e4194c
arixii35 <arinastepenko7@gmail.com>
2026-08-09 18:06:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Arix Studio");
+
+const heroVisual = document.getElementById('heroVisual');
+
+if (heroVisual) {
+    heroVisual.addEventListener('mousemove', (e) => {
+        const rect = heroVisual.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        heroVisual.style.setProperty('--x', `${x}px`);
+        heroVisual.style.setProperty('--y', `${y}px`);
+    });
+}
